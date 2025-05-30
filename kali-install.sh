cb28ed990900be10bf4c952c783dd5de0c6aa6bc
ghoste9624 <jackspade44@outlook.com>
2025-05-30 00:55:22 -0400

    Update kali-install.shnano

--- kali-install.sh
+++ kali-install.sh
@@ -1,5 +1,5 @@
 #1/bin/bash                                
-sudo apt update && sudo apt upgrade -y && sudo apt install whois nmap curl dnsutils binutils traceroute vim python3 python3 display-dhammapada -y                                    
+sudo apt update && sudo apt upgrade -y && sudo apt install whois nmap curl dnsutils binutils traceroute nano python3 python3 display-dhammapada -y                                    
 cd ~/
 rm dominium
 cd ~/Dominium                                    
